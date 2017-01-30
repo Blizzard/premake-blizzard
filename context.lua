01f2c8104fb2af730c7715fdd2a85b3bd7c171bb
Tom van Dijck <tvandijck@blizzard.com>
2017-01-30 14:39:27 -0800

    [blz] Fix the default folders I tried to make work last year, but never got figured out.

--- context.lua
+++ context.lua
@@ -5,19 +5,26 @@
 
 bnet = bnet or {}
 
+newoption
+{
+    trigger = 'to',
+    default = 'build',
+    value   = 'path',
+    description = 'Set the output location for the generated files'
+}
+
 bnet.build_custom_variant = nil
-bnet.build_dir    = path.join(_MAIN_SCRIPT_DIR, _OPTIONS["to"])
+bnet.build_dir    = path.join(_MAIN_SCRIPT_DIR, _OPTIONS.to or 'build')
+bnet.projects_dir = path.join(bnet.build_dir, 'projects')
 bnet.bin_dir      = path.join(_MAIN_SCRIPT_DIR, "bin/%{cfg.buildcfg}")
-bnet.obj_dir      = "%{bnet.build_dir}/%{_build_variant(cfg)}/obj"
-bnet.lib_dir      = "%{bnet.build_dir}/%{_build_variant(cfg)}/lib"
-bnet.projects_dir = "%{bnet.build_dir}/projects"
+bnet.obj_dir      = path.join(bnet.build_dir, "%{_build_variant(cfg)}/obj")
+bnet.lib_dir      = path.join(bnet.build_dir, "%{_build_variant(cfg)}/lib")
 
 verbosef("bnet.build_dir   : %s", bnet.build_dir)
+verbosef("bnet.projects_dir: %s", bnet.projects_dir)
 verbosef("bnet.obj_dir     : %s", bnet.obj_dir)
 verbosef("bnet.lib_dir     : %s", bnet.lib_dir)
 verbosef("bnet.bin_dir     : %s", bnet.bin_dir)
-verbosef("bnet.projects_dir: %s", bnet.projects_dir)
-
 
 function bnet.new()
 	return {
