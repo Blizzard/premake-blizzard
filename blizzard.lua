f5d638df09ab7a134f9f9df973efe9da55f35b8a
Tom van Dijck <tvandijck@blizzard.com>
2017-06-21 10:11:36 -0700

    Move some overrides from core into blizzard module.

--- blizzard.lua
+++ blizzard.lua
@@ -17,7 +17,7 @@
 		include('export.lua')
 		include('telemetry.lua')
 
-		-- provide overrides here.
+		-- Add additional systems and it's tags.
 		p.api.addAllowed('system', {'centos6', 'centos7', 'ubuntu'})
 
 		p.override(os, "getSystemTags", function (base, name)
@@ -30,6 +30,29 @@
 			return tags[name] or base(name)
 		end)
 
+		-- SC2/Heroes and some package need the Unicode flag for a while longer.
+		p.api.addAllowed('flags', {'Unicode'})
+
+		p.api.deprecateValue("flags", "Unicode", 'Use `characterset "Unicode"` instead',
+			function(value)
+				characterset "Unicode"
+			end,
+			function(value)
+				characterset "Default"
+			end
+		)
+
+		-- SC2/Heroes and some packages need the "buildoutputsasinputs" api still.
+		p.api.register {
+			name = "buildoutputsasinputs",
+			scope = "config",
+			kind = "boolean"
+		}
+
+		p.api.deprecateField("buildoutputsasinputs", nil, function(value)
+			p.warn("buildoutputsasinputs has been deprecated, please use 'compilebuildoutputs' instead.")
+			compilebuildoutputs(value)
+		end)
 	end
 
 	return p.modules.blizzard
